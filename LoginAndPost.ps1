96a9d1a1c9d532554f99a76ed7cc17efa912a5c2
Chris <49737464+chrispyth42@users.noreply.github.com>
2019-09-30 09:47:19 -0700

    Update LoginAndPost.ps1
    It's basically done now c:

--- LoginAndPost.ps1
+++ LoginAndPost.ps1
@@ -2,13 +2,10 @@
 $IE= new-object -ComObject "InternetExplorer.Application"
 $IE.visible = $true
 
+#Returns 0 if login failed, 1 if login successful, 2 if a user is already logged in
 function login{
-    #Request credentials from the user, and exit with 0 if they cancel
-    try{
-        $c = Get-Credential
-    }catch{
-        return 0
-    }
+    #User credentials variable
+    $c = ''
 
     #Navigate to reddit frontpage
     $IE.navigate2('https://old.reddit.com/')
@@ -23,6 +20,13 @@ function login{
 
     #If fields exist (Indicating that nobody's signed in), sign in with the provided credentials
     if($username){
+        #Request credentials from the user, and exit with 0 if they cancel
+        try{
+            $c = Get-Credential
+        }catch{
+            return 0
+        }
+
         #Set the username and password fields
         $username.value = $c.UserName
         $password.value = $c.GetNetworkCredential().Password #Password as plain text
@@ -37,22 +41,36 @@ function login{
             Start-Sleep -Milliseconds 100
         }
     }else{
+        #Get the name of currently signed in user by pulling their name from the a href "/user/username/saved"
+        $user = ''
+        $a = $IE.Document.IHTMLDocument3_getElementsByTagName("a")
+        foreach($l in $a){
+            if($l.href -match "/user/[^/]+/saved"){
+                $user = $l.href.split('/')[4]
+            }
+        }
+        Write-Host "----------------------------"
+        Write-Host("Posting as $user")
         return 2
     }
 
-    #Verify the login
-    $check = $IE.Document.IHTMLDocument3_getElementsByTagName("div")
-    if($check | Where-Object -FilterScript {($_.innerHTML -eq 'wrong password') -or ($_.innerHTML -eq 'incorrect username or password')}){
+    #Verify the login (If the login field goes away, that shows the login was successful)
+    $check = $IE.Document.IHTMLDocument3_getElementsByTagName("input")
+    if($check | Where-Object -FilterScript {$_.name -eq 'user'}){
+        Write-Host("Login Unsuccessful")
         return 0
     }else{
+        Write-Host "----------------------------"
+        Write-Host("Posting as " + $c.UserName)
         return 1
     }
 }
 
+#Returns 0 on failure, 1 on success
 function selfPost{
     param([string]$Title,[string]$SelfText,[String]$Subreddit)
     
-    #Go to post page
+    #Initiate shitposting
     $IE.navigate2('https://old.reddit.com/submit?selftext=true')
     while ($IE.busy){
         Start-Sleep -Milliseconds 1000
@@ -72,15 +90,25 @@ function selfPost{
     ($btn | Where-Object -FilterScript {($_.name -eq 'submit')}).Click()
 
     #Wait for loading to finish
+    Start-Sleep -Milliseconds 1000
     while ($IE.busy){
         Start-Sleep -Milliseconds 100
     }
+
+    #If the submit button didn't redirect the page to a new post, the post failed and retuns 0
+    if($IE.LocationURL -eq 'https://old.reddit.com/submit?selftext=true'){
+        Write-Host "Post Unsuccessful"
+        return 0
+    }else{
+        Write-Host "Post made: " $IE.LocationURL
+        return 1
+    }
 }
 
+#Returns 0 on failure, 1 on success
 function linkPost{
     param([string]$Title,[string]$URL,[String]$Subreddit)
-    
-    #Go to post page
+    #Initiate shitposting
     $IE.navigate2('https://old.reddit.com/submit')
     while ($IE.busy){
         Start-Sleep -Milliseconds 1000
@@ -100,22 +128,56 @@ function linkPost{
     ($btn | Where-Object -FilterScript {($_.name -eq 'submit')}).Click()
 
     #Wait for loading to finish
+    Start-Sleep -Milliseconds 1000
     while ($IE.busy){
         Start-Sleep -Milliseconds 100
+    }
+    
+    #If the submit button didn't redirect the page to a new post, the post failed and retuns 0
+    if($IE.LocationURL -eq 'https://old.reddit.com/submit'){
+        Write-Host "Post Unsuccessful"
+        return 0
+    }else{
+        Write-Host "Post made: " $IE.LocationURL
+        return 1
+    }
+}
+
+#Prompts the user for information to post to reddit
+function newPost{
+    $mode = Read-Host -Prompt 'Self(0) or link(1)?'
+    $title = Read-Host -Prompt 'Post title'
+    $subreddit = Read-Host -Prompt 'Subreddit'
+    $url_body = Read-Host -Prompt 'Post text or URL'
+    return ($mode, $title, $subreddit, $url_body)
+}
+
+#Prompts the user for post data
+function makePost{
+    $post = newPost
+    if($post[0] -eq 0){
+        selfPost -Title $post[1] -SelfText $post[3] -Subreddit $post[2]
+    }else{
+        linkPost -Title $post[1] -URL $post[3] -Subreddit $post[2]
     }
 }
 
 #Log in to reddit
 $loggedIn = login
 
-#If login was successful, make a post
-if($loggedIn -eq 1){
-    $title = "If you could re-do today from the moment you woke up, what would you do differently?"
-    $body = ""
-    $sub = 'askreddit'
-    selfPost -Title $title -SelfText $body -Subreddit $sub
-
-#Else, write a failure message and quit internet explorer
+#If logged in, continue to making the post
+if($loggedIn){
+    #Make post and prompt the user foe consecutive posts
+    Write-Host "----------New Post----------"
+    $p = makePost
+    $again = Read-Host -Prompt 'Make another post? (y/n)'
+    
+    #Continue until user declines to make another post
+    while(($again -eq 'y') -or ($again -eq 'Y')){
+        Write-Host "----------New Post----------"
+        $p = makePost
+        $again = Read-Host -Prompt 'Make another post? (y/n)'
+    }
 }else{
     Write-Host("Invalid Username/Password")
     $IE.Quit()
